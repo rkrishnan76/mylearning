7aae2961f718fb5f2d08b6558e5ef877138f36fa
rkrishnan76 <rkrishnan76_us@yahoo.com>
2017-12-01 20:50:48 -0800

    Update HourGlassProblem.java

--- HourGlassProblem.java
+++ HourGlassProblem.java
@@ -24,9 +24,9 @@ public class HourGlassProblem {
             }
                     for (int i = 0; i < 4; i++) {
                         for (int j = 0; j < 4; j++) {
-                              int tempSum = arr[i][j] + arr[i][j+1] + arr[i][j+2]
-                                    + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1]
-                                    + arr[i+2][j+2];
+                              int tempSum = arr[i][j]   + arr[i][j+1]   + arr[i][j+2]
+                                                        + arr[i+1][j+1] +
+                                            arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
                             if(maxSum<tempSum)
                                 maxSum=tempSum;
 
