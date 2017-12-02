b63cbac2b762512e72bcc05a1f84f0a543394d04
rkrishnan76 <rkrishnan76_us@yahoo.com>
2017-12-01 19:46:53 -0800

    Update BinaryNumber.java

--- BinaryNumber.java
+++ BinaryNumber.java
@@ -9,10 +9,10 @@ public class BinaryNumber {
         int i = scan.nextInt();
         int numOfConsecutiveOnes=0;
         boolean reset=true;
-        int temp=0;
+        int tempCount=0;
         while(true){
-            if(i%2==1 && ++temp>numOfConsecutiveOnes) numOfConsecutiveOnes=temp;
-            if(i%2==0) temp =0;
+            if(i%2==1 && ++tempCount>numOfConsecutiveOnes) numOfConsecutiveOnes=tempCount;
+            if(i%2==0) tempCount =0;
             i = i/2;
             if(i==0) break;
         }
